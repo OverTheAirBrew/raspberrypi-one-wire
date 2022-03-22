0b352ae5c9de673b806beec4641eda3c4f618a56
Nick Sharp <nick.sharp.87@gmail.com>
2022-03-22 12:08:36 +0000

    fix: exposing interface

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,3 @@
-export * from './lib/controller/ds18B20'
-export * from './lib/controller/stream'
+export * from "./lib/controller/ds18B20";
+export * from "./lib/controller/stream";
+export * from "./lib/i-one-wire-controller";
